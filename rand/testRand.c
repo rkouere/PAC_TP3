f795b993779cc876fc25f1ac2d03835bacaf688e
rkouere <rkouere@gmail.com>
2015-03-29 10:56:56 +0200

    test avec la fonction rand de c... ne marche pas

--- rand/testRand.c
+++ rand/testRand.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  int seed = atoi(argv[1]);
-  srand(seed);
-  printf("%d\n", rand());
+  int i = 0;
+  int IV_0 = 9004;
+  int IV_1 = 29395;
+
+  for(i = 100000000; i < 2^32; i++) {
+    if((i % 1000000) == 0)
+      printf("Testing seed = %d\n", i);
+    srand(i);
+    if(rand() == IV_0) {
+      printf("=========================\n");
+      printf("Possible seed = %d\n", i);
+      printf("=========================\n");
+    }
+  }
 }
